fdcad11a8c4045fe430c51a79af4fc7624a0012f
arun.kumar <arun.kumar@scrapehero.com>
2024-11-23 06:56:53 +0530

    neccessary files added

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # End-End-Medical-chatbot
 Medical chatbot using llm
+
+
+# Steps to run the project
+
+```
+create a new environment
+```
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Medical-chatbot',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A GEN AI chatbot from llama2',
+    author='ARUN KUMAR K',
+    author_email="arunaswin3583@gmail.com"
+)
